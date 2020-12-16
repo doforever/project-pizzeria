918f4da6f78d5d58da9ec840efcea5a744b7cf6d
Dorota Tomala <doforever@gmail.com>
2020-12-17 00:07:20 +0100

    Initialize date and hour wigdets at Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(container) {
@@ -14,6 +16,9 @@ class Booking {
 
     this.dom.peopleAmount = this.dom.wrapper.querySelector(select.booking.peopleAmount);
     this.dom.hoursAmount = this.dom.wrapper.querySelector(select.booking.hoursAmount);
+    this.dom.datePicker = this.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    this.dom.hourPicker = this.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+
   }
   initWidgets() {
     this.peopleAmount = new AmountWidget (this.dom.peopleAmount);
@@ -24,6 +29,8 @@ class Booking {
     this.dom.hoursAmount.addEventListener('updated', () => {
 
     });
+    this.datePicker = new DatePicker(this.dom.datePicker);
+    this.hourPicker = new HourPicker(this.dom.hourPicker);
   }
 }
 
